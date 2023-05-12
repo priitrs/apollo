1a80be06ddbcff3f91229327925650bf8563f510
priitrs <priitrs@gmail.com>
2023-05-12 07:26:50 +0300

    Add stocks endpoint

--- api/stocks.js
+++ api/stocks.js
@@ -1,3 +1,8 @@
-const stocks = {"LHV": "4.012"}
-
-export default stocks;
+export default function handler(request, response) {
+    const stocks = {
+        LHV: "4.012",
+        MERKO: "3.5",
+        COOP: "1.2"
+    };
+response.status(200).json({stocks})
+}
